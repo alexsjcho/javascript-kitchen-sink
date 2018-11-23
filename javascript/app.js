14b138ccbc6cd9f1b7ebee7f05c17450bfe679af
Alex Cho <alexsjcho@gmail.com>
2018-11-23 10:23:45 -0800

    2nd commit

---
+++ javascript/app.js
@@ -0,0 +1,30 @@
+//Variable that contains the value name Alex
+let name = "Alex";
+//Variable that contains the number of states in U.S.
+const states = '50';
+//Variable that computes arithmetic
+var add = 5 + 4;
+
+//sayHello Function
+function sayHello("Hello World!") {
+    console.log(sayHello)
+}
+
+//Function checkAge with 2 Arguments with Age and Name
+let checkAge = function(name, age){
+    name = "Charles, Abby, James, John";
+    age = "21, 27, 18, 17";
+    if (age < 21) {
+        alert("Welcome to this page!");
+    } else {
+        alert("Sorry ${name} you aren\'t old enough to view this page!");
+    }
+    console.log(checkAge.name.age);
+}
+
+//Vegetable arrays
+let vegetables = ["Carrots", "Potatoe", "Beans"];
+    for (i in vegetables) {
+        i++;
+        return vegetable
+    }
